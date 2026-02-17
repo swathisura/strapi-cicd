a0b547b8aad79e46741d74877ba538e8faaca3e7
swathisura <swathisura65@gmail.com>
2026-02-17 12:03:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,9 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_instance" "strapi" {
-  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (Mumbai)
   instance_type = "t2.micro"
-  key_name      = "strapi-key"
+  key_name      = "strapi-key" # <-- change if needed
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -14,11 +12,37 @@ resource "aws_instance" "strapi" {
               service docker start
               usermod -aG docker ec2-user
 
-              docker pull ${var.image}
-              docker run -d -p 80:1337 ${var.image}
+              docker pull ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/REPO:latest
+              docker run -d -p 80:1337 ACCOUNT_ID.dkr.ecr.ap-south-1.amazonaws.com/REPO:latest
               EOF
 
   tags = {
     Name = "Strapi-Server"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "strapi_sg" {
+  name = "strapi-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
